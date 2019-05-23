bf9606cd6e42eacbb82d91346fdf6ec23cadf580
Darlan Cavalcante Moreira <darcamo@gmail.com>
2019-05-23 11:30:01 -0300

    Add all remaining operations

--- operations.py
+++ operations.py
@@ -1,5 +1,7 @@
-import unittest
+# pylint: disable=C0111
+# pylint: disable=W0212
 
+import unittest
 
 
 def negative_decorator(function):
@@ -32,6 +34,23 @@ def add_digits(number, digits):
     return int(str(number) + str(digits))
 
 
+# @negative_decorator
+def warp(number, enter_idx, exit_idx=0):
+    factor = 10**exit_idx
+    digits = str(number)
+    warped_digits = digits[:-enter_idx]
+    non_warped_digits = digits[-enter_idx:]
+    out = int(non_warped_digits)
+    for warped_digit in warped_digits:
+        out = out + factor * int(warped_digit)
+
+    # Reapply warp recursively until it does not change the result
+    if out != number:
+        out = warp(out, enter_idx, exit_idx)
+
+    return out
+
+
 @negative_decorator
 def reverse(number):
     return int(reverse_as_str(number))
@@ -39,6 +58,8 @@ def reverse(number):
 
 @negative_decorator
 def mirror(number):
+    if number > 999:
+        raise ValueError("Too large")
     return int("{}{}".format(number, reverse_as_str(number)))
 
 
@@ -48,7 +69,7 @@ def replace(value, old, new):
 
 def divideby(numerator, denominator):
     if numerator % denominator != 0:
-        raise RuntimeError("Invalid int division")
+        raise ValueError("Invalid int division")
     return numerator // denominator
 
 
@@ -56,7 +77,7 @@ def divideby(numerator, denominator):
 def shift_left(value):
     number = str(value)
     if len(number) == 1:
-        raise RuntimeError("Can only shift numbers with at least two digits")
+        raise ValueError("Can only shift numbers with at least two digits")
     return int(number[:-1])
 
 
@@ -77,8 +98,21 @@ def sum_digits(number):
     return sum([int(i) for i in str(number)])
 
 
+@negative_decorator
+def inv10_each_digit(number):
+    if number == 0:
+        return 0
+
+    if number < 10:
+        # Number only has one digit
+        return abs(10-number)
+
+    digits = str(number)
+    return "".join([str(inv10_each_digit(int(i))) for i in digits])
+
+
 class Operation:
-    def __init__(self, name, op):
+    def __init__(self, name):
         """
         Parameters
         ----------
@@ -88,17 +122,19 @@ class Operation:
             A function performing the operation
         """
         self._name = name
-        self._op = op
 
     @property
     def name(self):
-        return self._name
+        return self._compute_name()
 
     def apply(self, number):
-        new_value = self._op(number)
+        new_value = self._apply_imp(number)
         if new_value == number:
-            raise RuntimeError("Useless operation")
+            raise ValueError("Useless operation")
         return new_value
+
+    def _apply_imp(self, number):
+        raise NotImplementedError("Implement-me")
 
     def _compute_name(self):
         return self._name
@@ -111,66 +147,106 @@ class Operation:
 class MultiplyX(Operation):
     def __init__(self, value):
         self._value = value
-        super().__init__("multiply by {}", lambda x: self._value * x)
+        super().__init__("multiply by {}")
+
+    def _apply_imp(self, x):
+        return self._value * x
 
 
 @class_with_value_decorator
 class DivideX(Operation):
     def __init__(self, value):
         self._value = value
-        super().__init__("divide by {}", lambda x: divideby(x, self._value))
+        super().__init__("divide by {}")
+
+    def _apply_imp(self, x):
+        return divideby(x, self._value)
 
 
 @class_with_value_decorator
 class SumX(Operation):
     def __init__(self, value):
         self._value = value
-        super().__init__("sum with {}", lambda x: self._value + x)
+        super().__init__("sum with {}")
+
+    def _apply_imp(self, x):
+        return self._value + x
 
 
 class Reverse(Operation):
     def __init__(self):
-        super().__init__("reverse", reverse)
+        super().__init__("reverse")
+
+    def _apply_imp(self, number):
+        return reverse(number)
 
 
 class Mirror(Operation):
     def __init__(self):
-        super().__init__("mirror", mirror)
+        super().__init__("mirror")
+
+    def _apply_imp(self, number):
+        return mirror(number)
 
 
 class Replace(Operation):
     def __init__(self, old, new):
         self._old = old
         self._new = new
-        super().__init__(f"Replace {old} with {new}", lambda x: replace(x, old, new))
+        super().__init__(f"Replace {old} with {new}")
+
+    def _apply_imp(self, x):
+        return replace(x, self._old, self._new)
 
 
 class CircularShiftRight(Operation):
     def __init__(self):
-        super().__init__("shift right", circular_shift_right)
+        super().__init__("shift right")
+
+    def _apply_imp(self, value):
+        return circular_shift_right(value)
 
 
 class CircularShiftLeft(Operation):
     def __init__(self):
-        super().__init__("shift left", circular_shift_left)
+        super().__init__("shift left")
+
+    def _apply_imp(self, number):
+        return circular_shift_left(number)
 
 
 class ShiftLeft(Operation):
     def __init__(self):
-        super().__init__("<<", shift_left)
+        super().__init__("<<")
+
+    def _apply_imp(self, number):
+        return shift_left(number)
+
 
 class SumDigits(Operation):
     def __init__(self):
-        super().__init__("Sum", sum_digits)
+        super().__init__("Sum")
+
+    def _apply_imp(self, number):
+        return sum_digits(number)
+
+
+class InvertSign(Operation):
+    def __init__(self):
+        super().__init__("+-")
+
+    def _apply_imp(self, value):
+        return -value
 
 
 class ModifyButtons_AddValue(Operation):
     def __init__(self, operations, value):
-        self._operations = [i for i in operations if hasattr(i, "increment_value_by")]
+        self._operations = [
+            i for i in operations if hasattr(i, "increment_value_by")]
         self._value = value
-        super().__init__("[+]{}", self._modify_buttons)
+        super().__init__("[+]{}")
 
-    def _modify_buttons(self):
+    def _apply_imp(self, number):
         for op in self._operations:
             op.increment_value_by(self._value)
 
@@ -178,15 +254,50 @@ class ModifyButtons_AddValue(Operation):
         return self._name.format(self._value)
 
     def apply(self, number):
-        self._op()
+        self._apply_imp(number)
         return number
+
+
+class StorageAction(Operation):
+    def __init__(self):
+        super().__init__("Store")
+
+
+class RetrieveAction(Operation):
+    def __init__(self):
+        super().__init__("Retrieve")
 
 
 @class_with_value_decorator
 class AddDigits(Operation):
     def __init__(self, value):
         self._value = value
-        super().__init__("Add digit {}", lambda x: add_digits(x, self._value))
+        super().__init__("Add digit {}")
+
+    def _apply_imp(self, x):
+        return add_digits(x, self._value)
+
+
+class Inv10EachDigit(Operation):
+    def __init__(self):
+        super().__init__("Inv10")
+
+    def _apply_imp(self, number):
+        return inv10_each_digit(number)
+
+
+class WarpAction(Operation):
+    """
+    Special action that can be added to a node and is automatically called
+    after every action without decrementing the number of moves
+    """
+    def __init__(self, enter_idx, exit_idx):
+        self._enter_idx = enter_idx
+        self._exit_idx = exit_idx
+        super().__init__("Warp")
+
+    def apply(self, number):
+        return warp(number, self._enter_idx, self._exit_idx)
 
 
 class Tests(unittest.TestCase):
@@ -199,21 +310,22 @@ class Tests(unittest.TestCase):
         self.assertEqual(mirror(-123), -123321)
         self.assertEqual(mirror(20), 2002)
 
-    def test_op_class(self):
-        r = Operation("reverse", reverse)
-        self.assertEqual(r.apply(246), 642)
-        self.assertEqual(r.name, "reverse")
+    # def test_op_class(self):
+    #     r = Operation("reverse", reverse)
+    #     self.assertEqual(r.apply(246), 642)
+    #     self.assertEqual(r.name, "reverse")
 
     def test_replace(self):
         self.assertEqual(replace(-12345, 34, 99), -12995)
         self.assertEqual(replace(12345, 12, 00), 345)
+        self.assertEqual(replace(2331, 31, "00"), 2300)
 
     def test_divideby(self):
         self.assertEqual(divideby(15, 3), 5)
         self.assertEqual(divideby(-15, 3), -5)
         self.assertEqual(divideby(15, -3), -5)
         self.assertEqual(divideby(-15, -3), 5)
-        with self.assertRaises(RuntimeError):
+        with self.assertRaises(ValueError):
             divideby(10, 3)
 
     def test_circular_shift_right(self):
@@ -242,6 +354,22 @@ class Tests(unittest.TestCase):
         self.assertEqual(add_digits(0, 5), 5)
         self.assertEqual(add_digits(5, 0), 50)
 
+    def test_inv10(self):
+        self.assertEqual(inv10_each_digit(0), 0)
+        self.assertEqual(inv10_each_digit(1), 9)
+        self.assertEqual(inv10_each_digit(6), 4)
+        self.assertEqual(inv10_each_digit(13), 97)
+        self.assertEqual(inv10_each_digit(-13), -97)
+        self.assertEqual(inv10_each_digit(30), 70)
+
+    def test_warp(self):
+        # Number in position 3 (1 in this case) is warped to position 0 and sum
+        # with what is in position 0 (4 in this case)
+        self.assertEqual(warp(1234, 3, 0), 235)
+        self.assertEqual(warp(6123, 3, 0), 129)
+        self.assertEqual(warp(991, 2, 0), 1)
+        self.assertEqual(warp(255255, 3, 1), 375)
+
     def test_ModifyButtons_AddValue(self):
         s = SumX(4)
         self.assertEqual(s._value, 4)
@@ -251,6 +379,7 @@ class Tests(unittest.TestCase):
         m.apply(99999)
         self.assertEqual(s._value, 4+9)
         self.assertEqual(s.apply(20), 20+13)
+
 
 # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 if __name__ == "__main__":
--- solver.py
+++ solver.py
@@ -1,6 +1,160 @@
 """Module docstring"""
 
+# pylint: disable=C0111
+# pylint: disable=W0212
+# pylint: disable=R0913
+
+import copy
+import unittest
 import operations as op
+
+
+class Node:
+    def __init__(self, value, current_op, available_ops, num_remaining_moves,
+                 parent=None, memory=None, warp=None):
+        """
+        data : tuple[int, list, list]
+        value : int
+            The current value
+        current_op : A subclass of op.Operation
+            The operation that was applied to get the current value.
+        available_ops : list[op.Operation]
+           The list of operations that this node can apply to get child nodes.
+        num_remaining_moves : int
+            Number remaining moves (children are only created if
+            num_remaining_moves > 0)
+        parent : Node
+            The parent Node
+        memory : int
+            Valued stored in the last call to the StorageAction. If not
+            provided and parent was provided then it will be taken from parent.
+        warp : op.WarpAction
+            Warp action to be called after every action. Note that the warp
+            action does not decrement the number of remaining moves.
+        """
+        self._value = value
+        self._current_op = current_op
+        self._available_ops = available_ops
+
+        self._num_remaining_moves = num_remaining_moves
+
+        self._children = []
+        self._parent = parent
+
+        self._memory = memory
+        self._warp = warp
+
+        # If memory was not provided but parent was provided, then we take the
+        # memory from parent
+        if parent is not None and memory is None:
+            self._memory = parent._memory
+
+    def __repr__(self):
+        return (f"Node(value={self._value},"
+                f"current_op={self._current_op},"
+                f"num_children={len(self._children)},"
+                f"rem_moves={self._num_remaining_moves},"
+                f"ops={self._available_ops})")
+
+    @property
+    def value(self):
+        return self._value
+
+    @property
+    def num_remaining_moves(self):
+        return self._num_remaining_moves
+
+    @property
+    def parent(self):
+        return self._parent
+
+    @property
+    def current_op(self):
+        return self._current_op
+
+    @staticmethod
+    def apply_operation_and_create_child(node, operation):
+        """Apply a Operation to the node and create a new node as the result
+
+        Parameters
+        ----------
+        node : Node
+            The Node object to apply the operation to
+        operation : op.Operation
+            The operation to apply to the Node object
+
+        Returns
+        -------
+        Node
+            The node resulting from applying `operation` to `node`
+        """
+        # __init__(self, value, current_op, available_ops, num_remaining_moves,
+        # parent=None)
+        parent = node
+        memory = None
+        num_remaining_moves = node.num_remaining_moves - 1
+        available_ops = node._available_ops
+        warp = node._warp
+
+        if isinstance(operation, op.ModifyButtons_AddValue):
+            available_ops = [copy.copy(i) for i in node._available_ops
+                             if i is not operation]
+            mb = op.ModifyButtons_AddValue(available_ops, operation._value)
+            available_ops.append(mb)
+            value = mb.apply(node.value)
+        elif isinstance(operation, op.StorageAction):
+            if node.value == node._memory:
+                raise ValueError("Can't store the same value")
+            value = node.value
+            num_remaining_moves = node.num_remaining_moves
+            memory = value
+
+        elif isinstance(operation, op.RetrieveAction):
+            value = op.add_digits(node.value, node._memory)
+        else:
+            value = operation.apply(node.value)
+
+        if warp is not None:
+            value = warp.apply(value)
+
+        return Node(value, operation, available_ops, num_remaining_moves,
+                    parent, memory, warp=warp)
+
+    def create_children(self):
+        if self._num_remaining_moves > 0:
+            for current_op in self._available_ops:
+                try:
+                    child = Node.apply_operation_and_create_child(
+                        self, current_op)
+                    self._children.append(child)
+                except ValueError:
+                    pass
+
+        for child in self._children:
+            child.create_children()
+
+
+def find_solution_node_in_tree(current_node, target_value):
+    if current_node.num_remaining_moves == 0:
+        if current_node.value == target_value:
+            return current_node
+        return None
+
+    for child in current_node._children:
+        value = find_solution_node_in_tree(child, target_value)
+        if value is not None:
+            return value
+    return None
+
+
+def parse_operations_until_node(node):
+    operations = []
+
+    while node is not None and node.current_op is not None:
+        operations.append(node.current_op.name)
+        node = node.parent
+
+    return list(reversed(operations))
 
 
 def apply_operations(value_and_ops, operations):
@@ -13,53 +167,167 @@ def apply_operations(value_and_ops, operations):
     for operation in operations:
         try:
             result.append((operation.apply(value), current_ops + [operation]))
-        except RuntimeError:
+        except ValueError:
             pass
     return result
     # return [(op.apply(value), current_ops + [op]) for op in operations]
 
 
+# xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+class TestSolver(unittest.TestCase):
+    @staticmethod
+    def solve(start_value, target_value, num_moves, operations, warp=None):
+        root = Node(value=start_value, current_op=None,
+                    available_ops=operations, num_remaining_moves=num_moves,
+                    warp=warp)
+        root.create_children()
+
+        n = find_solution_node_in_tree(root, target_value)
+        solution = parse_operations_until_node(n)
+        return solution
+
+    def test_level_127(self):
+        start_value = -1
+        target_value = 2020
+        num_moves = 8
+        operations = [op.MultiplyX(3),
+                      op.SumX(2),
+                      op.SumX(8),
+                      op.Mirror(),
+                      op.Reverse()]
+        solution = self.solve(start_value, target_value, num_moves, operations)
+        self.assertEqual(solution,
+                         ['sum with 8', 'multiply by 3', 'reverse',
+                          'sum with 8', 'mirror', 'sum with 2', 'sum with 8',
+                          'sum with 8'])
+
+    def test_level_139(self):
+        start_value = 5
+        target_value = 41
+        num_moves = 4
+
+        operations = [op.MultiplyX(3),
+                      op.SumX(4),
+                      op.SumX(8)]
+        mb = op.ModifyButtons_AddValue(operations, 2)
+        operations.append(mb)
+        solution = self.solve(start_value, target_value, num_moves, operations)
+        self.assertEqual(
+            solution,
+            ['[+]2', 'multiply by 5', 'sum with 6', 'sum with 10'])
+
+    def test_level_142(self):
+        start_value = 1
+        target_value = 1111
+        num_moves = 2
+
+        storage = op.StorageAction()
+        retrieve = op.RetrieveAction()
+
+        operations = [storage, retrieve]
+        solution = self.solve(start_value, target_value, num_moves, operations)
+        self.assertEqual(solution, ['Store', 'Retrieve', 'Store', 'Retrieve'])
+
+    def test_level_149(self):
+        start_value = 15
+        target_value = 16
+        num_moves = 4
+
+        storage = op.StorageAction()
+        retrieve = op.RetrieveAction()
+
+        operations = [
+            op.SumDigits(),
+            op.Replace(11, "33"),
+            op.Reverse(),
+            storage,
+            retrieve,
+        ]
+        solution = self.solve(start_value, target_value, num_moves, operations)
+        self.assertEqual(
+            solution,
+            ['Store', 'reverse', 'Retrieve', 'Replace 11 with 33', 'Sum'])
+
+    def test_solution_159(self):
+        start_value = 21
+        target_value = 12
+        num_moves = 3
+
+        operations = [
+            op.SumX(-7),
+            op.MultiplyX(5),
+            op.Inv10EachDigit()
+        ]
+        solution = self.solve(start_value, target_value, num_moves, operations)
+        self.assertEqual(solution, ['multiply by 5', 'sum with -7', 'Inv10'])
+
+    def test_solution_180(self):
+        # This level is the first one with Warp
+        start_value = 99
+        target_value = 10
+        num_moves = 3
+
+        operations = [
+            op.AddDigits(1),
+            op.SumX(-1),
+        ]
+
+        warp = op.WarpAction(2, 0)
+
+        solution = self.solve(start_value, target_value, num_moves, operations,
+                              warp)
+        self.assertEqual(solution, ['Add digit 1', 'Add digit 1', 'sum with -1'])
+
+
+if __name__ == '__main__1':
+    unittest.main()
+
+
 if __name__ == '__main__':
-    # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    ## O nível 127 foi o que me fez criar esse programa
-    # start_value = -1
-    # target_value = 2020
-    # num_moves = 8
+    # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    start_value = 3002
+    target_value = 3507
+    num_moves = 6
 
-    # operations = [op.MultiplyX(3),
-    #               op.SumX(2),
-    #               op.SumX(8),
-    #               op.Mirror(),
-    #               op.Reverse()]
-    # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    # +1 Storage Retrieve +1 Retrieve
 
-    # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    ## Some level
-    start_value = 0
-    target_value = 28
-    num_moves = 5
+    storage = op.StorageAction()
+    retrieve = op.RetrieveAction()
 
-    operations = [op.AddDigits(1),
-                  op.SumX(2),
-                  # op.SumX(2),
-                  # op.Mirror(),
-                  # op.Reverse()
-                  ]
+    operations = [
+        # op.SumX(6),
+        # op.MultiplyX(3),
+        # op.DivideX(2),
+        # op.ShiftLeft(),
+        op.CircularShiftRight(),
+        # op.InvertSign(),
+        op.AddDigits(7),
+        # op.SumDigits(),
+        op.Replace(3, "5"),
+        # op.Mirror(),
+        # op.CircularShiftRight(),
+        # op.Reverse(),
+        # op.SumDigits(),
+        # storage,
+        # retrieve,
+        # op.SumX(2),
+        # op.Mirror(),
+        # op.Reverse()
+        op.Inv10EachDigit()
+    ]
 
-    # mb = op.ModifyButtons_AddValue(operations, 3)
+    # mb = op.ModifyButtons_AddValue(operations, 1)
     # operations.append(mb)
-    # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-    values_and_ops = [(start_value, [])]
-    for move_idx in range(num_moves):
-        new_values_and_ops = []
-        for v_and_ops in values_and_ops:
-            new_values_and_ops.extend(apply_operations(v_and_ops, operations))
-        values_and_ops = new_values_and_ops
+    warp = None
+    warp = op.WarpAction(4, 0)
+    # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-    print("Tested plays: ", len(values_and_ops))
-    for i, ops in values_and_ops:
-        if i == target_value:
-            print(i)
-            print(ops)
-            # break
+    root = Node(value=start_value, current_op=None, available_ops=operations,
+                num_remaining_moves=num_moves, warp=warp)
+    root.create_children()
+
+    n = find_solution_node_in_tree(root, target_value)
+    operations = parse_operations_until_node(n)
+    print(operations)
